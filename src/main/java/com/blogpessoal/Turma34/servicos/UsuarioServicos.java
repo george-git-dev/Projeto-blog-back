8514f725a21ee267f3ab5b769a9208b0cc899869
George <george_roots@hotmail.com>
2021-11-17 00:01:40 -0300

    Fixing setFoto-UsuarioService

--- src/main/java/com/blogpessoal/Turma34/servicos/UsuarioServicos.java
+++ src/main/java/com/blogpessoal/Turma34/servicos/UsuarioServicos.java
@@ -123,8 +123,8 @@ public class UsuarioServicos {
 				objetoCredenciaisDTO.setNome(resp.getNome());
 				objetoCredenciaisDTO.setEmail(resp.getEmail());
 				objetoCredenciaisDTO.setSenha(resp.getSenha());
-				objetoCredenciaisDTO.setSenha(resp.getTipo());
-				objetoCredenciaisDTO.setSenha(resp.getFoto());
+				objetoCredenciaisDTO.setTipo(resp.getTipo());
+				objetoCredenciaisDTO.setFoto(resp.getFoto());
 
 				return ResponseEntity.status(201).body(objetoCredenciaisDTO); // Usuario Credenciado
 			} else {
